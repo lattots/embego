ba5b28a9a1e4168d37ed0cdc5bf221f2b093e6b3
otso <otso@lattu.biz>
2023-11-20 23:39:15 +0200

    Updated token frequency handling:
    - Token frequencies are inserted to database in batches of 500
    - Wrapped file closing in closure function to please linter
    - Improved token saving readability

--- pkg/parser/util.go
+++ pkg/parser/util.go
@@ -22,7 +22,12 @@ func readCatalog(filename string) (products []Product, err error) {
 	if err != nil {
 		return nil, err
 	}
-	defer jsonFile.Close()
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+
+		}
+	}(jsonFile)
 
 	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
@@ -39,14 +44,19 @@ func readCatalog(filename string) (products []Product, err error) {
 
 // Saves token frequencies to database. Returns error/nil.
 func saveTokenFrequency(db *gorm.DB, tokenCount TokenCount) error {
-	// All token counts are saved to the database provided.
+	// Convert token-count pairs to a slice of Token structs
+	tokens := make([]Token, 0, len(tokenCount))
 	for token, count := range tokenCount {
-		r := db.Create(&Token{Token: token, Count: count})
-		if r.Error != nil {
-			return r.Error
-		}
+		tokens = append(tokens, Token{Token: token, Count: count})
 	}
-	// If the save is successful, the funtion returns nil.
+
+	// Use CreateInBatch to perform bulk insert
+	r := db.CreateInBatches(tokens, 500)
+	if r.Error != nil {
+		return r.Error
+	}
+
+	// If the save is successful, the function returns nil.
 	return nil
 }
 
@@ -61,15 +71,28 @@ func formCorpus(products ProductCatalog) string {
 }
 
 func saveTokens(outputFilename string, tokens []string) error {
+	outputTXTFile, err := os.Create(outputFilename)
+	if err != nil {
+		return err
+	}
+
+	defer func(outputTXTFile *os.File) {
+		err := outputTXTFile.Close()
+		if err != nil {
+
+		}
+	}(outputTXTFile)
+
 	// Tokens are concatenated to single string
 	// Individual tokens are separated by space
 	tokensString := strings.Join(tokens, " ")
 
 	// Tokens are written to a text file
-	err := os.WriteFile(outputFilename, []byte(tokensString), 0644)
+	_, err = outputTXTFile.WriteString(tokensString)
 	if err != nil {
 		return err
 	}
-	// If the write is successful, the function returns nil
+
+	// If write is successful, the function returns nil
 	return nil
 }
